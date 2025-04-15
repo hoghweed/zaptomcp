89e450093217e4029cf638d411108ebbccba15c9
Manuel S. Martone <manuel@awellhealth.com>
2025-04-16 01:28:52 +0200

    test: 💍 streamable-http tests

--- packages/sdk/vitest.config.js
+++ packages/sdk/vitest.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
+        testTimeout: 6000,
         environmentMatchGlobs: [["src/**", ""]],
         exclude: ["node_modules", "dist", ".idea", ".git", ".cache", "src/main/**", "package.json"]
     }
